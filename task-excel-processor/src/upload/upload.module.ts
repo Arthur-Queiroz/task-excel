1eb7301f041e4aea3c9e14f959221a68bfb30551
Arthur-Queiroz <arthurdequeiroz2005@gmail.com>
2026-01-08 18:35:11 -0300

    Adicionando TasksUploadController ao módulo

--- task-excel-processor/src/upload/upload.module.ts
+++ task-excel-processor/src/upload/upload.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { ProjectsUploadController } from './projects-upload.controller';
+import { TasksUploadController } from './tasks-upload.controller';
 import { UploadService } from './upload.service';
 import { ExcelProcessor } from './excel.processor';
 import { TaskProcessor } from './task.processor';
 import { ApiService } from './services/api.service';
 
 @Module({
-  controllers: [UploadController, ProjectsUploadController],
+  controllers: [UploadController, ProjectsUploadController, TasksUploadController],
   providers: [UploadService, ExcelProcessor, TaskProcessor, ApiService],
 })
 export class UploadModule {}
